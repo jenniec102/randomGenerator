8ae6df8bb0dd4125aafae8755139e3f4a81f09a7
Jennifer <jenniec102@gmail.com>
2022-04-06 01:30:26 -0400

    fixed input

--- randomGenerator.js
+++ randomGenerator.js
@@ -15,11 +15,11 @@ submitButton.addEventListener("click", generate);
 function generate(){
   // inputs
   var day = document.getElementById("day").value;
-  var protein = document.getElementById("protein").value;
-  var carb = document.getElementById("carb").value;
-  var side1 = document.getElementById("side1").value;
-  var side2 = document.getElementById("side2").value;
-  var dessert = document.getElementById("dessert").value;
+  var protein = document.getElementById("protein").checked;
+  var carb = document.getElementById("carb").checked;
+  var side1 = document.getElementById("side1").checked;
+  var side2 = document.getElementById("side2").checked;
+  var dessert = document.getElementById("dessert").checked;
   // randomly choose each category
   var randProtein = Math.floor(Math.random()*proteins.length);
   var randCarb = Math.floor(Math.random()*carbs.length);
@@ -28,19 +28,46 @@ function generate(){
   var randDesserts = Math.floor(Math.random()*desserts.length);
 
   outputText.innerText = "Try this Meal for " + day + " :";
-  output.innerText = proteins[randProtein] + " " +  carbs[randCarb] + " " + sides[randSide1] + " " + sides[randSide2] + " " + desserts[randDesserts];
+
+  var finalOutputString = "| ";
+
+  if (protein) {
+    finalOutputString +=  proteins[randProtein] + " | ";
+  }
+  if (carb) {
+    finalOutputString +=  carbs[randCarb] + " | ";
+  }
+  if (side1) {
+    finalOutputString +=  sides[randSide1] + " | ";
+  }
+  if (side2) {
+    finalOutputString +=  sides[randSide2] + " | ";
+  }
+  if (dessert) {
+    finalOutputString +=  desserts[randDesserts] + " | ";
+  }
+
+  output.innerText = finalOutputString;
+
+  restyle();
+}
+
+function restyle(){
+  output.style.textShadow = "0.8px 0.8px 0 white, -0.8px 0.8px 0 white, 0.8px -0.8px 0 white, -0.8px -0.8px 0 white";
 
   var textDeco = ["blink", "line-through", "overline", "underline"];
   randDeco = Math.floor(Math.random()*textDeco.length);
-  outputText.style.textDecoration = "text-decoration: " + textDeco[randDeco];
+  output.style.textDecoration = "text-decoration: " + textDeco[randDeco];
+
+  var randSize = Math.floor(Math.random()*3);
+  randSize += 1; //font size will always be greater than or equal to 1
+  output.style.fontSize = randSize + "em";
 
   var randR = Math.random()*255;
   var randG = Math.random()*255;
   var randB = Math.random()*255;
-  outputText.style.backgroundColor = "rgb(" + randR + randG + randB + ")"
+  output.style.backgroundColor = "rgb(" + randR + "," + randG + "," + randB + ")" //add commas
 
-  var randPad = Math.random()*5;
-  outputText.style.padding = randPad + "vmin"; 
+  var randPad = Math.random()*8;
+  output.style.padding = randPad + "vmin";
 }
-
-// console.log(day, protein, carb, side1, side2, dessert);
